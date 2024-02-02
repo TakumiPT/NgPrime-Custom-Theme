f1b6abb8e79f105ec14fdd9aa308d7784904541e
filipefreire <filipefreire@ext.worten.pt>
2024-02-02 15:55:26 +0000

    Add layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,15 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-import { FormsModule } from '@angular/forms';
-
-
-import { InputSwitchModule } from 'primeng/inputswitch';
-import {  ButtonModule } from 'primeng/button';
+import { Component, OnInit } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet, InputSwitchModule, FormsModule, ButtonModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    templateUrl: './app.component.html'
 })
-export class AppComponent {
-  public checked: any;
+export class AppComponent implements OnInit {
+
+    constructor(private primengConfig: PrimeNGConfig) { }
+
+    ngOnInit() {
+        this.primengConfig.ripple = true;
+    }
 }
