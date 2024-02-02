68ad1285caf9e69368c787401d235f0207d070f8
filipefreire <filipefreire@ext.worten.pt>
2024-02-02 15:13:49 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+
+import { InputSwitchModule } from 'primeng/inputswitch';
+import {  ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, InputSwitchModule, FormsModule, ButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'ng-prime-sass';
+  public checked: any;
 }
